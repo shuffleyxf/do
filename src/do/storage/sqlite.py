dd7a9d89fc95e1d1dbadb5877df23c99eb6ff374
yangxf <shuffleyxf@gmail.com>
2023-06-11 00:59:09 +0800

    增加基于sqlite的存储器实现

---
+++ src/do/storage/sqlite.py
@@ -0,0 +1,149 @@
+import json
+import sqlite3
+from contextlib import contextmanager
+from sqlite3 import Cursor
+from typing import Union
+
+from storage import Storage, FailedTask, TaskState, TaskType
+
+
+class SqliteStorage(Storage):
+    """基于sqlite的任务存储器"""
+    _TB_NAME = 'failed_task'    # 表名
+    _PK = 'task_id' # 主键名
+
+    def __init__(self, db='do.db') -> None:
+        super().__init__()
+        self._db = db
+        self._init_db()
+
+    @contextmanager
+    def _new_conn(self):
+        conn = sqlite3.connect(self._db)
+        try:
+            yield conn
+            conn.commit()
+        except Exception:
+            conn.close()
+            raise
+
+    def _init_db(self):
+        with self._new_conn() as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS `{self._TB_NAME}`(
+                task_id INTEGER PRIMARY KEY,
+                task_type INTEGER,
+                task_name TEXT,
+                task_args TEXT,
+                task_kwargs TEXT,
+                runner_name TEXT,
+                retry_count INTEGER,
+                max_retry INTEGER,
+                create_time FLOAT,
+                update_time FLOAT,
+                state INTEGER
+            )
+            """)
+
+    @staticmethod
+    def _to_data_dict(task: FailedTask) -> dict:
+        return {
+            'task_type': int(task.task_type),
+            'task_name': task.task_name,
+            'task_args': json.dumps({'task_args': task.task_args}),
+            'task_kwargs': json.dumps(task.task_kwargs),
+            'runner_name': task.runner_name,
+            'retry_count': task.retry_count,
+            'max_retry': task.max_retry,
+            'create_time': task.create_time,
+            'update_time': task.update_time,
+            'state': int(task.state)
+        }
+
+    @staticmethod
+    def _to_task(data_dict: dict) -> FailedTask:
+        return FailedTask(
+            task_id=data_dict.get('task_id'),
+            task_type=TaskType(data_dict.get('task_type')),
+            task_name=data_dict.get('task_name'),
+            task_args=json.loads(data_dict.get('task_args'))['task_args'],
+            task_kwargs=json.loads(data_dict.get('task_kwargs')),
+            runner_name=data_dict.get('runner_name'),
+            retry_count=data_dict.get('retry_count'),
+            max_retry=data_dict.get('max_retry'),
+            create_time=data_dict.get('create_time'),
+            update_time=data_dict.get('update_time'),
+            state=TaskState(data_dict.get('state'))
+        )
+
+    def _select(self, cursor: Cursor, condition: str = "") -> [FailedTask]:
+        keys = ['task_id', 'task_type', 'task_name', 'task_args', 'task_kwargs', 'runner_name',
+                'retry_count', 'max_retry', 'create_time', 'update_time', 'state']
+        select_sql = f"SELECT "
+        for k in keys:
+            select_sql += f"{k}, "
+        select_sql = f'{select_sql.rstrip(", ")} FROM {self._TB_NAME} {condition}'
+        cursor.execute(select_sql)
+        values = cursor.fetchall()
+        task_list = list()
+        data_dict = dict()
+        for value in values:
+            for i, k in enumerate(keys):
+                data_dict[k] = value[i]
+            task_list.append(self._to_task(data_dict))
+        return task_list
+
+    def take(self) -> Union[FailedTask, None]:
+        with self._new_conn() as conn:
+            cursor = conn.cursor()
+            task_list = self._select(cursor, f"""
+                WHERE state = 1
+                ORDER BY UPDATE_TIME
+                LIMIT 1
+            """)
+            if task_list:
+                return task_list[0]
+            return None
+
+    def put(self, task: FailedTask) -> None:
+        with self._new_conn() as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"""
+            SELECT * FROM `{self._TB_NAME}`
+            WHERE task_id = {task.task_id}
+            """)
+            rows = cursor.fetchall()
+            exists = len(rows) > 0
+            data_dict = self._to_data_dict(task)
+            values = list()
+            if exists:
+                update_sql = f"UPDATE `{self._TB_NAME}` SET "
+                for k in ['task_name', 'task_args', 'task_kwargs', 'retry_count', 'update_time', 'state']:
+                    update_sql += f"{k} = ?, "
+                    values.append(data_dict.get(k))
+                update_sql = update_sql.rstrip(", ")
+                cursor.execute(update_sql, values)
+            else:
+                insert_sql = f"INSERT INTO `{self._TB_NAME}`("
+                key_counter = 0
+                for k, v in data_dict.items():
+                    if k == self._PK:
+                        continue
+                    insert_sql += f"{k}, "
+                    values.append(data_dict.get(k))
+                    key_counter += 1
+                insert_sql = f"{insert_sql.rstrip(', ')}) VALUES(" \
+                             + (key_counter - 1) * "?, " \
+                             + "?)"
+                cursor.execute(insert_sql, values)
+
+    def remove(self, task_id: int) -> None:
+        with self._new_conn() as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"DELETE FROM `{self._TB_NAME}` WHERE {self._PK} = {task_id}")
+
+    def all(self) -> [FailedTask]:
+        with self._new_conn() as conn:
+            cursor = conn.cursor()
+            return self._select(cursor)
--- tests/test_api.py
+++ tests/test_api.py
@@ -7,6 +7,7 @@ from error import DoException
 from model import BaseNamer
 from storage import TaskType
 from confest import start_do
+from storage.sqlite import SqliteStorage
 
 
 class TestDo1:
@@ -21,7 +22,6 @@ class TestDo1:
             raise Exception("not 66")
         self.data['66'] = True
 
-    @do
     def do_get_66(self):
         self.data['counter-do'] += 1
         if self.data['counter-do'] != 66:
@@ -29,6 +29,8 @@ class TestDo1:
         self.data['66-do'] = True
 
     def test_case(self, start_do):
+        self.do_get_66 = do(self.do_get_66)
+
         self.data.update({
             '66': False,
             'counter': 0,
@@ -61,7 +63,6 @@ class TestDo2:
             raise Exception("not 66")
         self.data['66'] = True
 
-    @do(max_retry=10)
     def do_get_66(self):
         self.data['counter-do'] += 1
         if self.data['counter-do'] != 66:
@@ -69,6 +70,8 @@ class TestDo2:
         self.data['66-do'] = True
 
     def test_case(self, start_do):
+        self.do_get_66 = do(self.do_get_66, max_retry=10)
+
         self.data.update({
             '66': False,
             'counter': 0,
@@ -96,7 +99,6 @@ class TestDo3:
     """
     data = dict()
 
-    @do(task_type=TaskType.NonIdempotent)
     def do_get_66(self):
         self.data['counter-do'] += 1
         if self.data['counter-do'] != 66:
@@ -104,6 +106,8 @@ class TestDo3:
         self.data['66-do'] = True
 
     def test_case(self, start_do):
+        self.do_get_66 = do(self.do_get_66, task_type=TaskType.NonIdempotent)
+
         self.data.update({
             '66-do': False,
             'counter-do': 0
@@ -124,14 +128,12 @@ class TestDo4:
     """
     data = dict()
 
-    @do(runner_name="do_10_get_66")
     def do_get_66(self):
         self.data['counter-do'] += 1
         if self.data['counter-do'] != 66:
             raise Exception("not 66")
         self.data['66-do'] = True
 
-    @do
     def do_10_get_66(self):
         self.data['counter-do'] += 1
         if self.data['counter-do'] != 66:
@@ -139,6 +141,9 @@ class TestDo4:
         self.data['66-do'] = False
 
     def test_case(self, start_do):
+        self.do_get_66 = do(self.do_get_66, runner_name="do_10_get_66")
+        self.do_10_get_66 = do(self.do_10_get_66)
+
         self.data.update({
             '66-do': False,
             'counter-do': 0
@@ -162,15 +167,16 @@ class TestDo5:
         def gen(self, func: callable, args: list, kwargs: dict) -> str:
             return "CustomTask"
 
-    @do
     def do_never_success(self):
         raise Exception("failed")
 
-    @do(namer_cls=CustomizeNamer)
     def do_never_success_custom(self):
         raise Exception("failed")
 
     def test_case(self, start_do):
+        self.do_never_success = do(self.do_never_success)
+        self.do_never_success_custom = do(self.do_never_success_custom, namer_cls=self.CustomizeNamer)
+
         with pytest.raises(Exception):
             self.do_never_success()
         with pytest.raises(Exception):
@@ -180,3 +186,45 @@ class TestDo5:
         task_name_list = [task.get('task_name') for task in task_info()]
         assert "do_never_success" in task_name_list
         assert "CustomTask" in task_name_list
+
+
+class TestDo6:
+    """
+    测试基于sqlite的内存存储器
+    """
+    data = dict()
+
+    def get_66(self):
+        self.data['counter'] += 1
+        if self.data['counter'] != 66:
+            raise Exception("not 66")
+        self.data['66'] = True
+
+    def do_get_66(self):
+        print(time.time())
+        self.data['counter-do'] += 1
+        if self.data['counter-do'] != 66:
+            raise Exception("not 66")
+        self.data['66-do'] = True
+
+    def test_case(self, start_do):
+        self.do_get_66 = do(self.do_get_66)
+        configure(storage=SqliteStorage())
+        self.data.update({
+            '66': False,
+            'counter': 0,
+            '66-do': False,
+            'counter-do': 0
+        })
+
+        assert self.data['66'] is False
+        assert self.data['66-do'] is False
+
+        with pytest.raises(Exception):
+            self.get_66()
+        with pytest.raises(Exception):
+            self.do_get_66()
+        time.sleep(20)
+
+        assert self.data['66'] is False
+        assert self.data['66-do'] is True
